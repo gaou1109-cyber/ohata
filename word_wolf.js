1edb4b0a2a302f248e1d3f511df9aeb22241bc21
DESKTOP-0B95F89\ohatay <y-ohata@fubic.com>
2019-12-04 09:51:38 +0900

    最初のコミット

---
+++ word_wolf.js
@@ -0,0 +1,7 @@
+//最初の画面(player_view)以外は見れないようにする
+//selectの値をtitle_01に送り表示
+//①title01を表示後、次へを押すとtitle_02を表示
+//②title02を表示後、次へを押すとtitle_03を表示
+//①と②を人数分ロープさせる
+//select_timeを表示。トーク時間を選定して次へを押すとlast_selectを表示
+//はいを押すと「game_start」を表示
